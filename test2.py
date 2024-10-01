de23a19edf1f0b172bba6c9bce60599e92b4704d
dixhap <dixhap@gmail.com>
2024-10-01 17:13:28 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,9 @@
+import os
+
+my_variable1 = os.getenv("MY_VARIABLE1")
+my_variable2 = os.getenv("MY_VARIABLE2")
+my_variable3 = os.getenv("MY_VARIABLE3")
+
+print(f"My variable 1: {my_variable1}")
+print(f"My variable 2: {my_variable2}")
+print(f"My variable 3: {my_variable3}")
