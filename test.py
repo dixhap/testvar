24e2809a20763b2cd69a051008e98287dc308c80
dixhap <dixhap@gmail.com>
2024-10-01 12:24:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    arm_environment = os.environ.get("ARM_ENVIRONMENT", "public")
+    arm_environment = os.environ.get("ARM_ENVIRONMENT")
     print(f"ARM_ENVIRONMENT variable value: {arm_environment}")
 
 if __name__ == "__main__":
