bf18595084d16593e647a5e48de9860c891b166b
dixhap <dixhap@gmail.com>
2024-10-01 11:59:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,8 @@
 import os
-from _version import __version__
-from shared.logging import logger, tracer
 
+def main():
+    arm_environment = os.environ.get("ARM_ENVIRONMENT", "public")
+    print(f"ARM_ENVIRONMENT variable value: {arm_environment}")
 
-VERSION = __version__
-
-
-def get_config() -> dict:
-    with tracer.start_as_current_span("get_config"):
-        config = {}       
-        arm_environment = os.environ.get("ARM_ENVIRONMENT", "public")
-        print(f"ARM_ENVIRONMENT variable value: {arm_environment}")
-        config["arm_environment"] = arm_environment        
-    return config
+if __name__ == "__main__":
+    main()
