b364cf4c784ee48c9df33987c2c691a93776c292
dixhap <dixhap@gmail.com>
2024-10-01 11:50:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,5 +9,7 @@ VERSION = __version__
 def get_config() -> dict:
     with tracer.start_as_current_span("get_config"):
         config = {}       
-        config["arm_environment"] = os.environ.get("ARM_ENVIRONMENT", "public")        
+        arm_environment = os.environ.get("ARM_ENVIRONMENT", "public")
+        print(f"ARM_ENVIRONMENT variable value: {arm_environment}")
+        config["arm_environment"] = arm_environment        
     return config
